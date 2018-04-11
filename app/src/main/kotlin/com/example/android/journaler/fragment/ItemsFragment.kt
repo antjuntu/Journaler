65d04b4d84f5b6b2ecff1b1ed72a22685ca82dc8
antjuntu <antjuntu@users.noreply.github.com>
2018-04-11 13:03:39 +0300

    ItemsFragment added

---
+++ app/src/main/kotlin/com/example/android/journaler/fragment/ItemsFragment.kt
@@ -0,0 +1,10 @@
+package com.example.android.journaler.fragment
+
+import com.example.android.journaler.R
+
+
+class ItemsFragment : BaseFragment() {
+    override val logTag = "Items fragment"
+    override fun getLayout() = R.layout.fragmet_items
+
+}
