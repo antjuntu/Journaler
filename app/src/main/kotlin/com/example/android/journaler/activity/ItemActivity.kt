88096d70e4630947bab8ad4156592d0cddae316c
antjuntu <antjuntu@users.noreply.github.com>
2018-04-12 12:44:18 +0300

    AlertDialog added

--- app/src/main/kotlin/com/example/android/journaler/activity/ItemActivity.kt
+++ app/src/main/kotlin/com/example/android/journaler/activity/ItemActivity.kt
@@ -1,9 +1,20 @@
 package com.example.android.journaler.activity
 
+import android.os.Bundle
+import android.util.Log
 import com.example.android.journaler.R
+import com.example.android.journaler.model.MODE
 
 
 abstract class ItemActivity : BaseActivity() {
 
+    protected var mode = MODE.VIEW
     override fun getActivityTitle() = R.string.app_name
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val modeToSet = intent.getIntExtra(MODE.EXTRAS_KEY, MODE.VIEW.mode)
+        mode = MODE.getByValue(modeToSet)
+        Log.v(tag, "Mode [ $mode ]")
+    }
 }
--- app/src/main/kotlin/com/example/android/journaler/fragment/ItemsFragment.kt
+++ app/src/main/kotlin/com/example/android/journaler/fragment/ItemsFragment.kt
@@ -1,10 +1,78 @@
 package com.example.android.journaler.fragment
 
+import android.app.AlertDialog
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.example.android.journaler.R
+import com.example.android.journaler.activity.NoteActivity
+import com.example.android.journaler.activity.TodoActivity
+import com.example.android.journaler.model.MODE
 
 
 class ItemsFragment : BaseFragment() {
     override val logTag = "Items fragment"
     override fun getLayout() = R.layout.fragmet_items
 
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater?.inflate(getLayout(), container, false)
+        val btn = view?.findViewById<FloatingActionButton>(R.id.new_item)
+
+        btn?.setOnClickListener {
+            val items = arrayOf(
+                getString(R.string.todos),
+                getString(R.string.notes)
+            )
+            val builder = AlertDialog.Builder(this@ItemsFragment.context)
+                .setTitle(R.string.choose_a_type)
+                .setItems(items, {
+                    _, which ->
+                    when(which) {
+                        0 -> openCreateTodo()
+                        1 -> openCreateNote()
+                        else -> Log.e(logTag, "Unknown option delected [ $which ]")
+                    }
+                })
+            builder.show()
+        }
+        return view
+    }
+
+    private fun openCreateNote() {
+        val intent = Intent(context, NoteActivity::class.java)
+        intent.putExtra(MODE.EXTRAS_KEY, MODE.CREATE.mode)
+        startActivity(intent)
+    }
+
+    private fun openCreateTodo() {
+        val intent = Intent(context, TodoActivity::class.java)
+        intent.putExtra(MODE.EXTRAS_KEY, MODE.CREATE.mode)
+        startActivity(intent)
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
